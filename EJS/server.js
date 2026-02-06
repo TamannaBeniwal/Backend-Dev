f191b30267ad2e5e147c1f987b723878d7bf59f8
tamanna <tamannabeniwal007@gmail.com>
2026-02-06 14:02:56 +0530

    classwork

--- EJS/server.js
+++ EJS/server.js
@@ -1,20 +1,73 @@
-import express from 'express';
+import express from "express";
+import methodOverride from "method-override"
 
 const app = express();
+
+app.use(methodOverride('_method'))
+
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index");
-})
 
-app.get("/user", (req, res) => {
-    let userData={
-        name:"amit",
-        age:23
-    }
-    res.render("user",  {userData});
+app.use(express.urlencoded({extended:true}))
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
+
+let userData = [
+  { id: 1, name: "amit", age: "23",},
+];
+
+//get edit page
+app.get("/editpage/:id",(req,res)=>{
+  const id = req.params.id;
+
+  res.render("edit")
+})
+
+// get user
+app.get("/user", (req, res) => {
+  res.render("user", { userData });
+});
+
+// add user
+app.post("/api/user",(req,res)=>{
+
+    const {name, age} = req.body;
+
+    let newUserData = {
+        id: userData.length+1,
+        name,
+        age
+    }
+    userData.push(newUserData);
+    res.redirect('/user')
+
+})
+
+//delete user
+app.delete("/api/user/:id",(req,res)=>{
+
+    const userid = req.params.id;
+
+    const useridx = userData.findIndex((ele)=> ele.id==userid);
+
+    if(useridx == -1){
+        return res.send("user not found")
+    }
+
+    userData.splice(useridx,1);
+
+    res.redirect("/user")
+
+})
+
+
+
+
+
 app.listen(3000, () => {
-    console.log("server is running on port 3000");
-}   );
+  console.log("server is running");
+});
